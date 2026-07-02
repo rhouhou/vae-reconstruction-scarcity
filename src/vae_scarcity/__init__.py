d4c731aef02d0a72cb0060671c3ece132e2e37f4
Rola Houhou <rola.h.houhou@gmail.com>
2026-07-02 14:15:16 +0200

    Add package metadata and import test

---
+++ src/vae_scarcity/__init__.py
@@ -0,0 +1,3 @@
+"""VAE reconstruction under sample scarcity."""
+
+__version__ = "0.1.0"
---
+++ tests/test_import.py
@@ -0,0 +1,4 @@
+def test_package_imports():
+    import vae_scarcity
+
+    assert vae_scarcity.__version__ == "0.1.0"
